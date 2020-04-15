36978d2105d3c3d3d7d7bba7802d4af4a5148b4e
Colin Gourlay <gourlay.colin@abc.net.au>
2020-04-15 13:32:48 +1000

    Restrict encoder props argument to plain objects. Everything else throws an error.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import baseX from 'base-x';
 import { Buffer } from 'buffer/';
+import isPlainObject from 'is-plain-object';
 import { TextEncoder } from 'text-encoding';
 
 const BASE_36_CHARSET = '0123456789abcdefghijklmnopqrstuvwxyz';
+const ERROR_NOT_PLAIN_OBJECT = 'props is not a plain object';
 
 const base36 = baseX(BASE_36_CHARSET);
 
 export function encode(props: Object): string {
+  if (!isPlainObject(props)) {
+    throw new Error(ERROR_NOT_PLAIN_OBJECT);
+  }
+
   return base36.encode(
     Buffer.from(new TextEncoder().encode(JSON.stringify(props)))
   );
