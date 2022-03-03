0ed712dbdb3916a26ebb6f037d2adcd87ab79404
Colin Gourlay <colin@colin-gourlay.com>
2022-03-03 11:35:50 +1000

    Use base-36-text as the encode/decoder

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,16 @@
-import 'fast-text-encoding';
-import baseX from 'base-x';
-import { Buffer } from 'buffer/';
+import { decode as _decode, encode as _encode } from '@abcnews/base-36-text';
 import { isPlainObject } from 'is-plain-object';
 
-const BASE_36_CHARSET = '0123456789abcdefghijklmnopqrstuvwxyz';
 const ERROR_NOT_PLAIN_OBJECT = 'props is not a plain object';
-
-const base36 = baseX(BASE_36_CHARSET);
 
 export function encode(props: Object): string {
   if (!isPlainObject(props)) {
     throw new Error(ERROR_NOT_PLAIN_OBJECT);
   }
 
-  return base36.encode(
-    Buffer.from(new TextEncoder().encode(JSON.stringify(props)))
-  );
+  return _encode(JSON.stringify(props));
 }
 
 export function decode<T extends Object>(base36string: string): T {
-  return JSON.parse(base36.decode(base36string).toString());
+  return JSON.parse(_decode(base36string));
 }
